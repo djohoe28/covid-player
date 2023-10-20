e92573b6087ffbfaf77d4718fb34b18a2e54a7dc
djohoe28 <djohoe28@gmail.com>
2023-10-20 11:46:48 +0300

    Server GET

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const cors = require("cors"); // Import the cors package
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
@@ -50,7 +51,9 @@ io.on("connection", (socket) => {
 });
 
 //
-
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "/index.html"));
+});
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
 	console.log(`Server is running on port ${port}...`);
