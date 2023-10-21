3590a2255ea5ce1bb4db25b76266f1b03687b337
djohoe28 <djohoe28@gmail.com>
2023-10-21 09:56:58 +0300

    (minor __dirname 4)

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors"; // Import the cors package
 import path from "path";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
+console.log(path.join(__dirname, "/index.html"));
 app.get("/", (req, res) => {
-	res.sendFile(path.join(path.dirname, "/index.html"));
+	res.sendFile(path.join(__dirname, "/index.html"));
+	
 });
-app.use(express.static(path.join(path.dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 const server = createServer(app);
 
 const CLIENT_PROT = process.env.PROT || "http";
@@ -16,7 +21,7 @@ const CLIENT_HOST = process.env.SELF || "localhost";
 const CLIENT_PORT = process.env.PORT || 10000; // TODO: Merge ports?
 const SERVER_PORT = process.env.PORT || 3000; // See above.
 const CLIENT_URL = new URL(`${CLIENT_PROT}://${CLIENT_HOST}:${CLIENT_PORT}`); // TODO: try-catch?
-const io = Server(server, {
+const io = new Server(server, {
 	cors: {
 		origin: process.env.CORS || process.env.SELF || CLIENT_URL.href || "*",
 	},
