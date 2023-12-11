d5ec1a51a68a0a29359ee1e9909d5d7bc03e6715
djohoe28 <djohoe28@gmail.com>
2023-12-11 11:10:18 +0200

    Removed Tailwind

--- src/app/components/UI/Button.jsx
+++ src/app/components/UI/Button.jsx
@@ -1,3 +1,3 @@
 export default function Button(props) {
-    return <input type="button" {...props} className={`rounded px-1 mx-0.5 dark:text-slate-300 dark:bg-slate-900 ${props.className ? props.className : ""}`} />
+    return <input type="button" {...props} className={`video-button ${props.className ? props.className : ""}`} />
 }
