c6ddcc762eb0547656f4ebe05a369c1f4624876a
djohoe28 <djohoe28@gmail.com>
2023-12-30 21:14:27 +0200

    VANILLA REFACTOR - DRAFT - CSS Grid implemented - Basic JS<>HTML implemented - Added a local video `src/video.mp4` for testing (also on .gitignore)

---
+++ src/main.js
@@ -0,0 +1,128 @@
+const MAX_DELTA = 1000; // Maximum video time difference (in miliseconds)
+
+const playVideo = document.getElementById("playVideo");
+const playSource = document.getElementById("playSource");
+const pauseButton = document.getElementById("pauseButton");
+const volumeInput = document.getElementById("volumeInput");
+const timeInput = document.getElementById("timeInput");
+const loadText = document.getElementById("loadText");
+const loadFile = document.getElementById("loadFile");
+const durationText = document.getElementById("durationText");
+const currentTimeText = document.getElementById("currentTimeText");
+const chatArea = document.getElementById("chatArea");
+const sendInput = document.getElementById("sendInput");
+const sendButton = document.getElementById("sendButton");
+
+function toHhMmSs(seconds) {
+    // TODO: Come up with more permanent / performant solution?
+	return new Date(seconds * 1000).toISOString().slice(11, 19);
+}
+
+function getState(props) {
+	let timestamp = new Date().getTime();
+	return {
+		// src: playSource.src, // TODO: Handle files/links differently? Send by default?
+		paused: playVideo.paused,
+		currentTime: playVideo.currentTime,
+		timestamp: timestamp,
+		...props,
+	};
+}
+
+function sendState(props) {
+	let state = getState(props);
+    console.log(state);
+    // TODO: Implement sending state to server.
+}
+
+function setState(state) {
+    // TODO: Implement receiving state from server.
+	let timestamp = new Date().getTime();
+	let latency = timestamp - state.timestamp; // NOTE: Delta-Time of packet sending/arrival in miliseconds
+	// TODO: Add video source change; prompt user for filename / automatically redirect video.src?
+	if (state.paused != playVideo.paused) {
+		state.paused ? playVideo.pause() : playVideo.play();
+	}
+	if (Math.abs(playVideo.currentTime - state.currenteTime) > MAX_DELTA) {
+		playVideo.currentTime = Math.max(
+			0,
+			Math.min(playVideo.duration, state.currentTime + latency)
+		); // NOTE: Clamp(0 < time < duration)
+	}
+}
+
+function getMessage(message) {
+	if (!message || message == "") {
+		return;
+	}
+    // TODO: Refactor to use a "message" modular element to allow sent/received messages.
+	let messageNode = document.createElement("p");
+	messageNode.textContent = message;
+	chatArea.appendChild(messageNode);
+	sendInput.value = "";
+}
+
+sendButton.onclick = function () {
+	getMessage(sendInput.value); // TODO: Add "sent" class? Redundant?
+};
+
+playVideo.ondurationchange = function () {
+	// Video duration changed
+	timeInput.max = playVideo.duration;
+	timeInput.step = playVideo.duration / 100;
+	durationText.innerHTML = toHhMmSs(playVideo.duration);
+};
+
+playVideo.ontimeupdate = function () {
+	// Video is playing
+	timeInput.value = playVideo.currentTime;
+	currentTimeText.innerHTML = toHhMmSs(playVideo.currentTime);
+};
+
+function togglePause() {
+	// Play/Pause toggled
+	let paused = !playVideo.paused; // Predictive
+	playVideo.paused ? playVideo.play() : playVideo.pause();
+	sendState({ paused: paused });
+}
+
+pauseButton.onclick = togglePause;
+playVideo.onclick = togglePause;
+
+volumeInput.oninput = function () {
+	// Volume input
+	playVideo.volume = volumeInput.value / 100;
+};
+
+timeInput.oninput = function () {
+	// currentTime input
+	playVideo.currentTime = timeInput.value;
+	sendState({ currentTime: timeInput.value });
+};
+
+loadText.onclick = function () {
+	// Load video link
+	let url = prompt("Enter video source address:");
+	playSource.src = url;
+	playVideo.load();
+    // TODO: Force source change?
+	sendState({
+		src: url,
+	});
+};
+
+loadFile.oninput = function () {
+	// lastModified: 1703012842204
+	// lastModifiedDate: Tue Dec 19 2023 21:07:22 GMT+0200 (Israel Standard Time) {}
+	// name: "Mori Calliope - Twitter Status 1693647928030875968 - 2023_08_21_18_35_00.mp4"
+	// size: 10572793
+	// type: "video/mp4"
+	// webkitRelativePath: ""
+    // TODO: How to identify video? Enforce identity? Unequal files =/= Unequal content.
+	let url = URL.createObjectURL(loadFile.files[0]);
+	playSource.src = url;
+	playVideo.load();
+	sendState({
+		src: loadFile.files[0].name,
+	});
+};
---
+++ src/sources.js
@@ -0,0 +1,16 @@
+export const sources = {
+	// Parasitic Worm - 446 x 336 (4:3 SD)
+	SD_04_03: "https://www.shutterstock.com/shutterstock/videos/5993084/preview/stock-footage-parasitic-worm-trichinella-spiralis-trichinosis-in-a-sample-of-raw-meat-muscle-fibers-under.webm",
+	// Fashion Woman - 360 x 640 (9:16 SD)
+	SD_09_16: "https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-fashion-woman-with-silver-makeup-39875-small.mp4",
+	// Big Buck Bunny - 426 x 240 (16:9)
+	_16_09: "https://samples.tdarr.io/api/v1/samples/sample__240__libvpx-vp9__aac__30s__video.mkv",
+	// Big Buck Bunny - 854 x 480 (16:9 SD)
+	SD_16_09: "https://samples.tdarr.io/api/v1/samples/sample__480__libvpx-vp9__aac__30s__video.mkv",
+	// Big Buck Bunny - 1280 x 720 (16:9 HD)
+	HD_16_09: "https://samples.tdarr.io/api/v1/samples/sample__720__libvpx-vp9__aac__30s__video.mkv",
+	// Big Buck Bunny - 1920 x 1080 (16:9 Full HD)
+	FHD_16_09: "https://samples.tdarr.io/api/v1/samples/sample__1080__libvpx-vp9__aac__30s__video.mkv",
+	// Big Buck Bunny - 3840 x 2160 (16:9 4K UHD)
+	UHD_16_09: "https://samples.tdarr.io/api/v1/samples/sample__2160__libvpx-vp9__aac__30s__video.mkv",
+}
