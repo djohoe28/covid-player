fff50ff3b23a7841d947675edf666a18dc4fde68
djohoe28 <djohoe28@gmail.com>
2023-12-31 05:51:32 +0200

    (minor) remove old index.ts

--- app.ts
+++ app.ts
@@ -9,3 +9,34 @@ const server = Bun.serve({
 });
 
 console.log(`[${NODE_ENV}] Serving http://localhost:${server.port}`);
+
+
+// const server = Bun.serve<{ username: string }>({
+//     fetch(req, server) {
+//       const cookies = req.headers.get("cookie");
+//       const username = new Date().toISOString(); // TODO: getUsernameFromCookies(cookies);
+//       const success = server.upgrade(req, { data: { username } });
+//       if (success) return undefined;
+  
+//       return new Response("Hello world");
+//     },
+//     websocket: {
+//       open(ws) {
+//         const msg = `${ws.data.username} has entered the chat`;
+//         ws.subscribe("the-group-chat");
+//         ws.publish("the-group-chat", msg);
+//       },
+//       message(ws, message) {
+//         // the server re-broadcasts incoming messages to everyone
+//         ws.publish("the-group-chat", `${ws.data.username}: ${message}`);
+//       },
+//       close(ws) {
+//         const msg = `${ws.data.username} has left the chat`;
+//         server.publish("the-group-chat", msg);
+//         ws.unsubscribe("the-group-chat");
+//       },
+//     },
+//   });
+  
+//   console.log(`Listening on ${server.hostname}:${server.port}`);
+  
